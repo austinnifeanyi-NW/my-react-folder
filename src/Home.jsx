58ff5bda43a3eb3f08a48e1e1daeb65093aa64bf
austinnifeanyi-NW <austinnifeanyi@gmail.com>
2026-06-18 11:33:46 +0100

    added smt

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='h-screen bg-sky-300 grid place-items-center text-5xl'>Home</div>
+    <div className='h-screen bg-sky-300 grid place-items-center text-5xl'>Home page</div>
     
   )
 }
